ae3952f22d41360f5da14f45d1b9f9c4b409f3b6
Mitch R <olordhux+github@gmail.com>
2014-09-13 02:37:40 -0700

    mixin to give an object a unique ID

---
+++ game/mixins/hasUniqueId.js
@@ -0,0 +1,15 @@
+hasUniqueId = function() {
+  var makeUUID = function() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+               .toString(16)
+               .substring(1);
+  };
+  
+  var uuid = makeUUID() + '_' + makeUUID();
+  
+  return {
+    uuid: uuid
+  };
+};
+
+module.exports = hasUniqueId;
