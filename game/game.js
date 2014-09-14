cb91c819e74c0b9c33155d397d18e9258705bce9
Mitch R <olordhux+github@gmail.com>
2014-09-14 01:06:24 -0700

    add ConnectControllers state to the game

--- game/game.js
+++ game/game.js
@@ -3,6 +3,7 @@ var game = new Phaser.Game(1024, 1024, Phaser.AUTO, 'game');
 game.state.add('Boot', require('./states/bootState'));
 game.state.add('Preloader', require('./states/preloaderState'));
 game.state.add('MainMenu', require('./states/mainMenuState'));
+game.state.add('ConnectControllers', require('./states/connectControllersState'));
 game.state.add('Play', require('./states/playState'));
 
 game.state.start('Boot');
