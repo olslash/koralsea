ef9c08a90d76d34c178a65d4542042adaff6974b
Mitch R <olordhux+github@gmail.com>
2014-09-14 23:50:42 -0700

    these functions should be on the state, not global scope

--- game/states/connectControllersState.js
+++ game/states/connectControllersState.js
@@ -9,21 +9,25 @@ ConnectControllers.create = function() {
   this.game.gameId = makeGameCode();
   this.game.socket = io('http://localhost:8000');
   this.game.socket.on('connect', function() {
+    
     var text = '' + this.game.gameId;
     var style = { font: "65px Arial", fill: "#ff0044", align: "center" };
-
     var t = this.game.add.text(this.game.world.centerX-100, 0, text, style);
 
     this.game.socket.emit('registerGame', this.game.gameId);
   }.bind(this));
 };
 
-function startGame() {
-  this.game.state.start('Play');
-}
+ConnectControllers.update = function() {
 
-function makeGameCode() {
+};
+
+ConnectControllers.startGame = function() {
+  this.game.state.start('Play');
+};
+
+ConnectControllers.makeGameCode = function() {
   return Math.floor(Math.random() * 99999);
-}
+};
 
 module.exports = ConnectControllers;
