33bc766d72307a20557339b1fc3d59d77cef8063
Mitch R <olordhux+github@gmail.com>
2014-09-15 01:19:00 -0700

    logic for controller connect/disconnect

--- game/states/connectControllersState.js
+++ game/states/connectControllersState.js
@@ -1,3 +1,5 @@
+var controllerService = require('../services/controllerService');
+
 var ConnectControllers = new Phaser.State();
 
 
@@ -18,20 +20,42 @@ ConnectControllers.create = function() {
   }.bind(this));
 
   this.game.socket.on('controller-connected', this.registerController);
+  this.game.socket.on('controller-disconnected', this.unregisterController);
+
+  this.startButton = this.add.button(100, 100, 'button-generic', this.startGame, this);
+  this.startButton.kill(); // todo: better syntax?
 };
 
+var t;
 ConnectControllers.update = function() {
+  var connectedPlayers = controllerService.controllers.length;
+  if(t === undefined) {
+    var text = connectedPlayers + ' players connected';
+    var style = { font: "32px Arial", fill: "#ff0044", align: "center" };
+    t = this.game.add.text(this.game.world.centerX, 100, text, style);  
+  }
+  // console.log(t);
+  t.setText(connectedPlayers + ' players connected');
 
+  if(connectedPlayers > 0 && !this.startButton.exists) {
+    this.startButton.revive();
+  } else if(connectedPlayers === 0 && this.startButton.exists) {
+    this.startButton.kill();
+  }
 };
 
-ConnectControllers.registerController = function () {
 
+ConnectControllers.registerController = function (controllerId) {
+  controllerService.registerController(controllerId);
+};
+
+ConnectControllers.unregisterController = function (controllerId) {
+  controllerService.unregisterController(controllerId);
 };
 
 ConnectControllers.startGame = function() {
   this.game.state.start('Play');
 };
-
 
 ConnectControllers.makeGameCode = function() {
   return Math.floor(Math.random() * 99999);
